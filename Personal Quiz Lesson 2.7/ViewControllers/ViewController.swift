93566444a14dd9350698ae69e95470116bd67abe
Kate Kashko <katekashkobiz@gmail.com>
2023-04-09 12:39:07 +0300

    Add Models - Question

--- Personal Quiz Lesson 2.7/ViewControllers/ViewController.swift
+++ Personal Quiz Lesson 2.7/ViewControllers/ViewController.swift
@@ -9,9 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let questions = Question.getQuestions()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
